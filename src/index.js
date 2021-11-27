fbb880b1b049bed30966dba5e098b02ade18a134
justinmavity <justinmavity1021@gmail.com>
2021-11-26 20:18:22 -0600

    made changes need in order to use redux with the reducers and actions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { carReducer } from './reducers/featureReducer'
 import 'bulma/css/bulma.css';
-import './styles.scss';
+import './styles.css';
+
+const store = createStore(
+    carReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
